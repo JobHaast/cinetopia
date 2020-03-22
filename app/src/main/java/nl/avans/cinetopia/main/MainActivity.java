824eedce17fe827aa589d94a79cf668c737d06ce
Job Haast <57725294+darkspeed002@users.noreply.github.com>
2020-03-22 18:14:59 +0100

    Begin recyclerview gemaakt

--- app/src/main/java/nl/avans/cinetopia/main/MainActivity.java
+++ app/src/main/java/nl/avans/cinetopia/main/MainActivity.java
@@ -2,6 +2,9 @@ package nl.avans.cinetopia.main;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String LIFECYCLE_CALLBACKS_TEXT_KEY = "callbacks";
 
     private ArrayList<Film> films = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+//    private FilmAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.activity_main_recyclerView);
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
+
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+//        adapter = new FilmAdapter(films, //enter handeler//);
+//        recyclerView.setLayoutManager(adapter);
     }
 
     @Override
