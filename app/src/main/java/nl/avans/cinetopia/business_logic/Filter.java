1a26fa784d582d57c2d9c9db71e0f5e23d5df6c9
NoahK <ns.korten@student.avans.nl>
2020-03-31 14:30:32 +0200

    added filter class

---
+++ app/src/main/java/nl/avans/cinetopia/business_logic/Filter.java
@@ -0,0 +1,37 @@
+package nl.avans.cinetopia.business_logic;
+
+public class Filter {
+
+    public static void filterRating(int ratingGroup) {
+        switch (ratingGroup) {
+            //If the ratingGroup is 1, the movies with ratings between 0 and 4 are loaded.
+            case 1:
+
+                break;
+
+            //If the ratingGroup is 2, the movies with ratings between 4 and 6 are loaded.
+            case 2:
+
+                break;
+
+            //If the ratingGroup is 3, the movies with ratings between 6 and 8 are loaded.
+            case 3:
+
+                break;
+            //If the ratingGroup is 4, the movies with ratings between 8 and 10.
+            case 4:
+
+                break;
+
+            default:
+
+                break;
+
+
+        }
+
+    }
+
+
+}
+
